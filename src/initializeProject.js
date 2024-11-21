d15110671d04f53331b5c9cc453946cd9f472039
gianlucaegallino <99291206+gianlucaegallino@users.noreply.github.com>
2024-11-21 08:22:08 -0300

    Add cookie reading and remove json importing

--- src/initializeProject.js
+++ src/initializeProject.js
@@ -10,8 +10,8 @@ function initializeProjects(){
     let todoobject = new toDo("Welcome!", "Enjoy your To-Do List!", format(new Date(), 'yyyy-MM-dd'), "High", true);
     projectList[0].addToDo(todoobject);
 
-    // Try read from LocalStorage
-    // project = readData(project);
+    //Attempt to read data from localstorage
+    projectList = readData(projectList);
     
     return projectList;
 }
